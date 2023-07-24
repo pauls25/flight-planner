90e2977ab1cd5ecfe52dfee7b2f863210b1074ae
pauls25 <76559243+pauls25@users.noreply.github.com>
2023-07-24 23:01:02 +0300

    correct create table scripts

---
+++ src/main/resources/db/changelog/changes/001-create-airports-flights-tables.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+--changeset pauls:1
+
+CREATE TABLE airports
+(
+    airport_id SERIAL PRIMARY KEY,
+    country varchar(255) NOT NULL,
+    city varchar(255) NOT NULL,
+    airport varchar(255) NOT NULL
+);
+
+CREATE TABLE flights
+(
+    flight_id SERIAL PRIMARY KEY ,
+    from_airport_id bigint,
+    to_airport_id bigint,
+    carrier varchar(255),
+    departure_time timestamp,
+    arrival_time timestamp,
+    FOREIGN KEY (from_airport_id) REFERENCES airports (airport_id),
+    FOREIGN KEY (to_airport_id) REFERENCES airports (airport_id)
+);
